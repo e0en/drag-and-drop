4fec8ef2aea8afa3c1b39598e12815e46f88abc2
Yoonseop Kang <e0engoon@gmail.com>
2019-12-29 13:25:13 +0900

    Reformat js/svelte files

--- src/DraggableItem.svelte
+++ src/DraggableItem.svelte
@@ -1,36 +1,36 @@
 <script>
-import { createEventDispatcher } from 'svelte'
+  import { createEventDispatcher } from "svelte"
 
-export let itemId
-export let isSelected = false
+  export let itemId
+  export let isSelected = false
 
-const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher()
 
-function mousedown_handler (ev) {
-    dispatch('selectItem', {itemId: itemId})
-}
+  function mousedown_handler(ev) {
+    dispatch("selectItem", { itemId: itemId })
+  }
 
-function mouseup_handler (ev) {
+  function mouseup_handler(ev) {
     isSelected = false
-}
-
+  }
 </script>
 
-<div class:selected="{isSelected}"
-    on:mousedown={mousedown_handler}
-    on:mouseup={mouseup_handler}>
-    Card {itemId} {isSelected}
-</div>
-
 <style>
-    div {
-        box-sizing: border-box;
-        border: 1px solid black;
-        margin: 1em;
-        padding: 1em;
-    }
+  div {
+    box-sizing: border-box;
+    border: 1px solid black;
+    margin: 1em;
+    padding: 1em;
+  }
 
-    .selected {
-        border: 2px solid red;
-    }
+  .selected {
+    border: 2px solid red;
+  }
 </style>
+
+<div
+  class:selected={isSelected}
+  on:mousedown={mousedown_handler}
+  on:mouseup={mouseup_handler}>
+  Card {itemId} {isSelected}
+</div>
